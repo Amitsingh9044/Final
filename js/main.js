c65a83e793a6c53627b93abc963bc28e0e0607a4
amitsingh9044 <amitsingh9044@gmil.com>
2024-09-07 13:39:47 +0530

    New Commit

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@
             if ($(window).width() > 992) {
                 $('.navbar .dropdown').on('mouseover', function () {
                     $('.dropdown-toggle', this).trigger('click');
+
                 }).on('mouseout', function () {
                     $('.dropdown-toggle', this).trigger('click').blur();
                 });
